188235bca579965b846f0c1eddf60c560941d9de
MoniVasileva <monivasileva2306@gmlai.com>
2023-03-24 12:15:05 +0200

    entities created

--- demo/src/main/java/com/example/demo/model/entities/Student.java
+++ demo/src/main/java/com/example/demo/model/entities/Student.java
@@ -1,12 +1,19 @@
 package com.example.demo.model.entities;
 
 import jakarta.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 @Table(name = "students")
-public class Student extends User{
+public class Student extends User {
     @ManyToOne
     private Speciality speciality;
+    @ManyToOne
+    private Subject subject;
+    private Double grade;
+
+    // private Map<Subject,Double> subjectGradeMap;
 
 
     public Speciality getSpeciality() {
@@ -15,6 +22,24 @@ public class Student extends User{
 
     public Student setSpeciality(Speciality speciality) {
         this.speciality = speciality;
+        return this;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public Student setSubject(Subject subject) {
+        this.subject = subject;
+        return this;
+    }
+
+    public Double getGrade() {
+        return grade;
+    }
+
+    public Student setGrade(Double grade) {
+        this.grade = grade;
         return this;
     }
 }
--- demo/src/main/java/com/example/demo/services/StudentService.java
+++ demo/src/main/java/com/example/demo/services/StudentService.java
@@ -1,8 +1,27 @@
 package com.example.demo.services;
 
+import com.example.demo.model.entities.Student;
+import com.example.demo.repositories.StudentRepository;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
 
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.
+                findAll().
+                stream().
+                toList();
+    }
+    public Optional<Student> getStudentById(Long studentId) {
+        return studentRepository.
+                findById(studentId);
+    }
 }
